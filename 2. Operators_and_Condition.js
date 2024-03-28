e46d7ccddd14af399247871de6f46373e88986c6
Swarup Padol <swarup.padol2010git config --global user.email swarup.padol2010@gmail.com>
2024-03-28 17:09:24 +0530

    Learing Arthemetic & Unary Operator

---
+++ 2. Operators_and_Condition.js
@@ -0,0 +1,27 @@
+/* Arithmetic Operators */
+
+let a = 5;
+let b = 2;
+
+console.log("a =", a, "& b = ", b);
+console.log("a - b =", a - b);
+console.log("a + b =", a + b);
+console.log("a × b =", a * b);
+console.log("a ÷ b =", a / b);
+
+// Modulus
+console.log("Remainder of a ÷ b =", a % b);
+
+// Power
+console.log("Answer to the a power b =", a ** b);
+
+// Unary Operator
+
+a = a + 1;
+a = a - 1;
+a++;
+a--;
+++a;
+--a;
+
+console.log("a = ", a);
