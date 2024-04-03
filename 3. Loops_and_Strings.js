17a704f15e0a0fc2e0824b36734b1a5fb021676f
Swarup Padol <swarup.padol2010git config --global user.email swarup.padol2010@gmail.com>
2024-04-03 17:40:05 +0530

    Learing some types of string's.

--- 3. Loops_and_Strings.js
+++ 3. Loops_and_Strings.js
@@ -1,57 +1,91 @@
-alert("Hello!");
-/* Loop's */
-// Loops are used to execute a piece of code again & again.
+// alert("Hello!");
+// /* Loop's */
+// // Loops are used to execute a piece of code again & again.
 
-// For Loop
-for (let i = 1; i <= 5; i++) {
-  console.log("i = ", i);
-}
+// // For Loop
+// for (let i = 1; i <= 5; i++) {
+//   console.log("i = ", i);
+// }
 
-let sum = 0;
-let count = 1;
-let user_number = prompt("Enter the number:");
+// let sum = 0;
+// let count = 1;
+// let user_number = prompt("Enter the number:");
 
-for (let count = 1; count <= user_number; count++) {
-  sum = sum + count;
-}
+// for (let count = 1; count <= user_number; count++) {
+//   sum = sum + count;
+// }
 
-console.log("sum = ", sum);
-console.log("loop has ended!");
+// console.log("sum = ", sum);
+// console.log("loop has ended!");
 
-// While loop
-let j = 1;
-while (j <= 5) {
-  console.log("j = ", j);
-  j++;
-}
+// // While loop
+// let j = 1;
+// while (j <= 5) {
+//   console.log("j = ", j);
+//   j++;
+// }
 
-// Do-While loop
-let k = 20;
-do {
-  console.log("k = ", k);
-  k++;
-} while (k <= 10);
+// // Do-While loop
+// let k = 20;
+// do {
+//   console.log("k = ", k);
+//   k++;
+// } while (k <= 10);
 
-// For-of loop
-let str = "Swarup";
-let size = 0;
+// // For-of loop
+// let str = "Swarup";
+// let size = 0;
 
-for (let val of str) {
-  // iterator -> Characters
-  console.log("val = ", val);
-  size++;
-}
+// for (let val of str) {
+//   // iterator -> Characters
+//   console.log("val = ", val);
+//   size++;
+// }
 
-console.log("size of string = ", size);
+// console.log("size of string = ", size);
 
-// For-in loop
-let student = {
-  fullName: "Rahul Kumar",
-  age: 20,
-  cgpa: 7.5,
-  isPass: true,
+// // For-in loop
+// let student = {
+//   fullName: "Rahul Kumar",
+//   age: 20,
+//   cgpa: 7.5,
+//   isPass: true,
+// };
+
+// for (const key in student) {
+//   console.log("key = ", key, "value = ", student[key]);
+// }
+
+/*Strings*/
+let firstName = "Swarup";
+let lastName = "Padol";
+
+console.log(firstName[4]); // u
+console.log(firstName[2]); // d
+
+// Template Literal
+let sentence = `This a Templete Litereal. ${1 + 2 + 3}`;
+
+console.log(sentence);
+console.log(typeof sentence);
+
+// Template Literal example
+let obj = {
+  item: "pen",
+  price: 10,
 };
 
-for (const key in student) {
-  console.log("key = ", key, "value = ", student[key]);
-}
+console.log(`The cost of the ${obj.item} is ₹${obj.price}.`); // String Interpolation
+console.log("The cost of", obj.item, "is ₹", obj.price, ".");
+
+// Escape Characters
+
+let esChar = console.log(
+  `Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iste officiis\n\tfugiat corrupti autem blanditiis totam\nsapiente repellat ipsa sunt, aspernatur vero aliquam earum impedit\n\ttemporibus qui, nihil modi nemo inventore culpa\naperiam? Debitis sit nostrum sunt commodi recusandae\n\tdeserunt sequi qui voluptas exercitationem quas. Ex eius\nvoluptatibus nostrum non`
+);
+
+//String Method's
+let strMethod = "       Chandrashekhar    ";
+let newStrMethod = strMethod.trim();
+
+console.log(newStrMethod);
